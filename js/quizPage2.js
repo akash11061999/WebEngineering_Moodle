f335913eb4d8d3c9ed39b951bf70f55f1c4447cb
Aayushi1506 <46044806+Aayushi1506@users.noreply.github.com>
2020-01-28 22:30:45 +0530

    quiz creating page 2 js file added

---
+++ js/quizPage2.js
@@ -0,0 +1,10 @@
+
+function showDiv1(){
+   document.getElementById('DIV1').style.display = "block";
+   document.getElementById('DIV2').style.display = "none";
+}
+function showDiv2(){
+   document.getElementById('DIV2').style.display = "block";
+   document.getElementById('DIV1').style.display = "none";
+}
+
