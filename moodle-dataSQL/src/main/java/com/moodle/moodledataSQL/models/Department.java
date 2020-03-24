a53fdb054f425665726fbec6d80a040cb9a220ae
aastha1602 <45291228+aastha1602@users.noreply.github.com>
2020-03-24 20:21:33 +0530

    Modified Department Model

--- moodle-dataSQL/src/main/java/com/moodle/moodledataSQL/models/Department.java
+++ moodle-dataSQL/src/main/java/com/moodle/moodledataSQL/models/Department.java
@@ -2,11 +2,15 @@ package com.moodle.moodledataSQL.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Department {
 	
+	@GeneratedValue( strategy=GenerationType.AUTO )
+
 	@Id
 	private String departmentId;
 	
